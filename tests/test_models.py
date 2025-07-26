f549b065a667f860387cd3a7973e5b30b9e6ceda
GatDeguin <gastonjorgecontreras@gmail.com>
2025-07-26 20:06:50 -0300

    Add unit tests and CI

---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+import torch
+from train import build_model
+
+
+def _run_forward(name: str):
+    model = build_model(name, num_classes=5)
+    inp = torch.randn(2, 3, 4, 544)
+    out = model(inp)
+    assert out.shape == (2, 4, 5)
+
+
+def test_stgcn_forward():
+    _run_forward("stgcn")
+
+
+def test_sttn_forward():
+    _run_forward("sttn")
+
+
+def test_corrnet_forward():
+    _run_forward("corrnet+")
